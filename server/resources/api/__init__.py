292a4ec64d896eb4400d5d3c7c6c28cd15b45aa8
Saakshaat <saakshaat2001@gmail.com>
2020-12-18 21:28:27 -0800

    Initialize session dependency in API init for use across routers

--- server/resources/api/__init__.py
+++ server/resources/api/__init__.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 main_router = APIRouter()
 
+from resources.db import session_dependency
+
+session_dep = session_dependency()
+
 
 @main_router.get("/", status_code=200)
 async def root():
