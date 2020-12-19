7d9986019123b3c59a1fbda2f457e182bb06bc85
Saakshaat <saakshaat2001@gmail.com>
2020-12-18 22:32:05 -0800

    Add User GET route for multiple users

--- server/resources/api/user.py
+++ server/resources/api/user.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter
 
 from models import user as user_model
@@ -16,3 +18,8 @@ async def create_single_user(user_data: user_schemas.UserPost, db=session_dep):
 @user_router.get("/user/{user_id}/", response_model=user_schemas.UserGet, status_code=200)
 async def get_single_user(user_id: int, db=session_dep):
     return read.read_single_resource(model=user_model.User, identifier='id', value=user_id, db=db)
+
+
+@user_router.get("/users/", response_model=List[user_schemas.UserGet], status_code=200)
+async def get_all_users(db=session_dep):
+    return read.read_multiple_resources(model=user_model.User, db=db)
