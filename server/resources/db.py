f4dd212c4f1ce14ba08245a3025bb7f28c7cd545
Saakshaat <saakshaat2001@gmail.com>
2020-12-18 19:08:21 -0800

    Add method for returning DB session dependency

---
+++ server/resources/db.py
@@ -0,0 +1,23 @@
+from fastapi import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+DATABASE_URL = 'sqlite:///memory'  # change to Postgres instance in docker-compose
+
+
+def session_dependency() -> Depends:
+    engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=0)
+
+    local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    def get_db_session():
+        db = local_session()
+        try:
+            return db
+        except:
+            raise RuntimeError('Error in starting DB')
+        finally:
+            db.close()
+
+    session_dep: Session = Depends(get_db_session())
+    return session_dep
