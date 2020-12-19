3d2fb2fabb4f5e8fa17ba04e4e5ae49e472f3c90
Saakshaat <saakshaat2001@gmail.com>
2020-12-18 22:42:47 -0800

    Add Contact model

---
+++ server/models/contact.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from .base import Model, IDMixin
+
+
+class Contact(Model, IDMixin):
+    user_id = Column(Integer, ForeignKey('user.id'))
+    discord = Column(String, nullable=True)
+    phone = Column(Integer, nullable=True)
+    snapchat = Column(String, nullable=True)
+    instagram = Column(String, nullable=True)
