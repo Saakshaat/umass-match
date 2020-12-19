f2606953d490b7060b06775c8b1fa4c33616725f
Saakshaat <saakshaat2001@gmail.com>
2020-12-18 20:08:21 -0800

    Fix sessions creation helper by creating all tables

--- server/resources/db.py
+++ server/resources/db.py
@@ -2,6 +2,8 @@ from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
+from models import Model
+
 DATABASE_URL = 'postgresql+psycopg2://staging_user:staging_password@database:5432/staging_db'
 
 
@@ -9,6 +11,8 @@ def session_dependency() -> Depends:
     engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=0)
 
     local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    Model.metadata.create_all(bind=engine)
 
     def get_db_session():
         db = local_session()
