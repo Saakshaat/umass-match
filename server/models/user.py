3a6c94826b6440d72e397f2ff1223e98b72a8f55
Saakshaat <saakshaat2001@gmail.com>
2020-12-18 21:14:10 -0800

    Update User model

--- server/models/user.py
+++ server/models/user.py
@@ -8,3 +8,7 @@ class User(Model, IDMixin):
     first_name = Column(String, nullable=False)
     middle_name = Column(String, nullable=True)
     last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    # contact (phone, discord, snapchat, instagram) = another model
+    # preferences = another model
+    # matches: self-referential (https://docs.sqlalchemy.org/en/14/orm/self_referential.html)
