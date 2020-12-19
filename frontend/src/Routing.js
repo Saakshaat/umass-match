015d1735d800b68d7fc332d74ada415bc12a7435
jackbisceglia <jackbisceglia2000@gmail.com>
2020-12-19 01:05:03 -0500

    base level components created

---
+++ frontend/src/Routing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from './App.js'
+import {BrowserRouter as Router} from "react-router-dom";
+
+
+function Routing() {
+  return (
+    // UPDATE FOR ROUTING LATER ON
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default Routing;
