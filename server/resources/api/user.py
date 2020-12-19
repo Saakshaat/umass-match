408d5bdabb60ff65be7129946d0acfa6a1bc4817
Saakshaat <saakshaat2001@gmail.com>
2020-12-18 22:11:34 -0800

    Use read helper in User GET roue

--- server/resources/api/user.py
+++ server/resources/api/user.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from models import user as user_model
-from resources.crud import create
+from resources.crud import create, read
 from schemas import user as user_schemas
 from . import session_dep
 
@@ -15,8 +15,4 @@ async def create_single_user(user_data: user_schemas.UserPost, db=session_dep):
 
 @user_router.get("/user/{user_id}/", response_model=user_schemas.UserGet, status_code=200)
 async def get_single_user(user_id: int, db=session_dep):
-    user = db.query(user_model.User).get(user_id)
-    if user is None:
-        raise HTTPException(status_code=404, detail=f'No user with id={user_id} found')
-
-    return user
+    return read.read_single_resource(model=user_model.User, identifier='id', value=user_id, db=db)
