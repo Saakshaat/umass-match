5f72969815e5596b74f2d7261d8e4155ba5b01c7
jackbisceglia <jackbisceglia2000@gmail.com>
2020-12-19 14:28:01 -0500

    Navbar created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Styles/app.css'
+import Navbar from './Components/Navbar'
 
 export default function App() {
     return (
         <div className="container">
-            <h1>Hello WORLD</h1>
+            <Navbar />
         </div>
     )
 }
