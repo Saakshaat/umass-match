92ad42758f3ca308cc172f50e96ca809fdfe6dbb
Saakshaat <saakshaat2001@gmail.com>
2020-12-19 21:07:54 -0800

    Fix and catch exceptions in match_users

--- server/resources/crud/custom.py
+++ server/resources/crud/custom.py
@@ -1,5 +1,7 @@
 import datetime
+import random
 
+from fastapi import HTTPException
 from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
@@ -10,7 +12,7 @@ from schemas.match import FilterParams
 def get_all_matchable_users(user_data, db: Session):
     """
     A `matchable` user is someone who has not been matched with the current user before and has not been
-    matched themself in the past 7 days.
+    matched themself in the past 3 days.
 
     :param user_data: the data of the current user
     :param db: database session
@@ -18,13 +20,13 @@ def get_all_matchable_users(user_data, db: Session):
     """
 
     current_time = datetime.datetime.now()
-    seven_days_ago = current_time - datetime.timedelta(seconds=10)  # TODO: revert to days
+    three_days_ago = current_time - datetime.timedelta(days=3)
 
     past_matched_ids_list = [match.other_user_id for match in user_data.previous_matches]
-    # an active user is someone who has not been matched in the past 7 days and
+    # an active user is someone who has not been matched in the past 3 days and
     matchable_users = db.query(User).filter(User.id != user_data.id).filter(User.id.notin_(
         past_matched_ids_list)).filter(or_(User.last_matched_time is None,
-                                           User.last_matched_time < seven_days_ago)).all()  # TODO: convert into subquery for builder pattern
+                                           User.last_matched_time < three_days_ago)).all()  # TODO: convert into subquery for builder pattern
 
     return matchable_users
 
@@ -48,7 +50,7 @@ class Filter:
             if bool(set(user.profile.majors) & set(self.user_data.profile.majors)):
                 filtered_users.append(user)
 
-        if len(filtered_users) == 0:
+        if not filtered_users:
             return self
 
         self.matchable_users = filtered_users
@@ -60,7 +62,7 @@ class Filter:
             if bool(set(user.profile.clubs) & set(self.user_data.profile.clubs)):
                 filtered_users.append(user)
 
-        if len(filtered_users) == 0:
+        if not filtered_users:
             return self
 
         self.matchable_users = filtered_users
@@ -72,7 +74,7 @@ class Filter:
             if bool(set(user.profile.umass_residences) & set(self.user_data.profile.umass_residences)):
                 filtered_users.append(user)
 
-        if len(filtered_users) == 0:
+        if not filtered_users:
             return self
 
         self.matchable_users = filtered_users
@@ -80,14 +82,14 @@ class Filter:
 
     def filter_by_grad_year(self):
         filtered_users = []
+
         for user in self.matchable_users:
             if user.profile.grad_year == self.user_data.profile.grad_year:
                 filtered_users.append(user)
 
-        if len(filtered_users) == 0:
-            return self
+        if filtered_users:
+            self.matchable_users = filtered_users
 
-        self.matchable_users = filtered_users
         return self
 
     def filter_by_video_games(self):
@@ -96,7 +98,7 @@ class Filter:
             if user.profile.video_games and self.user_data.profile.video_games:
                 filtered_users.append(user)
 
-        if len(filtered_users) == 0:
+        if not filtered_users:
             return self
 
         self.matchable_users = filtered_users
@@ -108,7 +110,7 @@ class Filter:
             if user.profile.music and self.user_data.profile.music:
                 filtered_users.append(user)
 
-        if len(filtered_users) == 0:
+        if not filtered_users:
             return self
 
         self.matchable_users = filtered_users
@@ -120,7 +122,7 @@ class Filter:
             if user.profile.movies and self.user_data.profile.movies:
                 filtered_users.append(user)
 
-        if len(filtered_users) == 0:
+        if not filtered_users:
             return self
 
         self.matchable_users = filtered_users
@@ -132,29 +134,33 @@ def match_user(user_id: int, filter_params: FilterParams, db: Session):
 
     matchable_users = get_all_matchable_users(user_data, db)
 
+    if not matchable_users:  # not possible matches currently
+        raise HTTPException(status_code=403, detail='All users matched for current user!')
+
     filtered = Filter(user_data=user_data,
                       matchable_users=matchable_users)
+
     if filter_params.majors:
-        filtered = filtered.filter_users_by_majors()
+        filtered.filter_users_by_majors()
 
     if filter_params.clubs:
-        filtered = filtered.filter_users_by_clubs()
+        filtered.filter_users_by_clubs()
 
     if filter_params.residences:
-        filtered = filtered.filter_users_by_residences()
+        filtered.filter_users_by_residences()
 
     if filter_params.grad_year:
-        filtered = filtered.filter_by_grad_year()
+        filtered.filter_by_grad_year()
 
     if filter_params.video_games:
-        filtered = filtered.filter_by_video_games()
+        filtered.filter_by_video_games()
 
     if filter_params.music:
-        filtered = filtered.filter_by_music()
+        filtered.filter_by_music()
 
     if filter_params.movies:
-        filtered = filtered.filter_by_movies()
+        filtered.filter_by_movies()
 
     filtered_users = filtered.matchable_users
 
-    return filtered_users
+    return random.choice(filtered_users)
