4b6c494178447fcba27b63a7e6070c0384691f73
Saakshaat <saakshaat2001@gmail.com>
2020-12-19 19:01:34 -0800

    Fix conditions for matching algorithm

--- server/resources/crud/custom.py
+++ server/resources/crud/custom.py
@@ -1,9 +1,13 @@
 import datetime
 
+from sqlalchemy import or_
+from sqlalchemy.orm import Session
+
+from models.profile import Profile
 from models.user import User
 
 
-def get_all_matchable_users(user_data, db):
+def get_all_matchable_users(user_data, db: Session):
     """
     A `matchable` user is someone who has not been matched with the current user before and has not been
     matched themself in the past 7 days.
@@ -14,18 +18,28 @@ def get_all_matchable_users(user_data, db):
     """
 
     current_time = datetime.datetime.now()
-    seven_days_ago = current_time - datetime.timedelta(minutes=1)
+    seven_days_ago = current_time - datetime.timedelta(seconds=10)  # TODO: revert to days
 
-    past_matched_ids_list = [match.id for match in user_data.previous_matches]
-
+    past_matched_ids_list = [match.other_user_id for match in user_data.previous_matches]
     # an active user is someone who has not been matched in the past 7 days and
-    matchable_users = db.query(User).filter(
-        User.last_matched_time > seven_days_ago and User.id not in past_matched_ids_list).all()
+    matchable_users = db.query(User).filter(User.id != user_data.id).filter(User.id.notin_(
+        past_matched_ids_list)).filter(or_(User.last_matched_time is None,
+                                           User.last_matched_time < seven_days_ago)).all()  # TODO: convert into subquery for builder pattern
 
     return matchable_users
 
 
-def match_user(user_id: int, db):
+def filter_users_by_profile(user_data, prev_subquery, db: Session):
+    filtered_users = db.query(User, prev_subquery).join(User.profile).filter(
+        bool(set(Profile.majors) & set(user_data.profile.majors))).subquery()
+
+    if len(filtered_users) == 0:
+        return db.query(User, prev_subquery).all()
+
+    return filtered_users
+
+
+def match_user(user_id: int, db: Session):
     user_data = db.query(User).get(user_id)
 
     matchable_users = get_all_matchable_users(user_data, db)
